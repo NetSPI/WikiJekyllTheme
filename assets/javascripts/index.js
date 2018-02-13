77788afc1c08c229f2babc46c970f467106d8a99
Jake Reynolds <jreynolds@netspi.com>
2018-02-12 22:04:42 -0600

    removing fast

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -29,72 +29,27 @@ $(document).ready(function() {
     if (isMobile) {
       closeNavigation();
       $('body').removeClass('noscroll');
-    } else if (getCookie('nav') == 'true' || getCookie('nav') == undefined) {
-      openNavigation();
-      $('body').removeClass('noscroll');
     }
   })
-
-  //Get a cookie value
-  function getCookie(name) {
-    var value = "; " + document.cookie;
-    var parts = value.split("; " + name + "=");
-    if (parts.length == 2) {
-      return parts.pop().split(";").shift();
-    } else {
-      return undefined;
-    }
-  }
-  if ((!getCookie('nav') && !isMobile) || getCookie('nav') == 'true') {
-    openNavigation(true);
-  }
 
   //Toggle the navigation
   function toggleNavigation() {
     $('.n-nav-icon').toggleClass('open');
     $('.n-nav-icon').toggleClass('active');
     $(".n-navbar").toggleClass('active');
-    if (!isMobile) { //Save the user's preference on desktop
-      document.cookie = "nav=" + $('.n-navbar').hasClass('active');
-    }
   }
 
   //Close the navigation
-  function closeNavigation(fast) {
-    if (fast) {
-      //A little overboard, but we have a lot of transitional elements
-      // that look weird if this snaps and they don't
-      $('*').addClass('no-transition');
-      $('.n-nav-icon').removeClass('open');
-      $('.n-nav-icon').removeClass('active');
-      $(".n-navbar").removeClass('active');
-      setTimeout(function() {
-        $('*').removeClass('no-transition');
-      }, 100)
-      $('body').removeClass('noscroll');
-    } else {
-      $('.n-nav-icon').removeClass('open');
-      $('.n-nav-icon').removeClass('active');
-      $(".n-navbar").removeClass('active');
-      $('body').removeClass('noscroll');
-    }
+  function closeNavigation() {
+    $('.n-nav-icon').removeClass('open');
+    $('.n-nav-icon').removeClass('active');
+    $(".n-navbar").removeClass('active');
+    $('body').removeClass('noscroll');
   }
   //Open the navigation
-  function openNavigation(fast) {
-    if (fast) {
-      //A little overboard, but we have a lot of transitional elements
-      // that look weird if this snaps and they don't
-      $('*').addClass('no-transition');
-      $('.n-nav-icon').addClass('open');
-      $('.n-nav-icon').addClass('active');
-      $('.n-navbar').addClass('active');
-      setTimeout(function() {
-        $('*').removeClass('no-transition');
-      }, 100)
-    } else {
-      $('.n-nav-icon').addClass('open');
-      $('.n-nav-icon').addClass('active');
-      $('.n-navbar').addClass('active');
-    }
+  function openNavigation() {
+    $('.n-nav-icon').addClass('open');
+    $('.n-nav-icon').addClass('active');
+    $('.n-navbar').addClass('active');
   }
 })
