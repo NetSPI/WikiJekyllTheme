0e1734d46cd1bbad9dfa26ffdeb24bd0a8cdbae2
Jake Reynolds <jreynolds@netspi.com>
2018-02-15 20:20:28 -0600

    updating tab hash handling

--- assets/javascripts/tab.js
+++ assets/javascripts/tab.js
@@ -10,11 +10,17 @@ $(document).ready(function() {
 
   //On forward/back buttons refresh everything
   $(window).on("popstate", function(e) {
-    $('#tabs .tabInput').attr('checked',false);
+    $('#tabs .tabInput').attr('checked', false);
     updateTab(null, true);
   });
 
+  //If there's no hash set a default one
   if (!window.location.hash) {
     window.location.hash = $(".tabs .tabInput")[0].id;
-  }
+  } else {
+    //If the hash we have doesn't exist, keep it, but just show another tab
+    if (!$(".tabs .tabInput#" + window.location.hash.replace('#', '')).length) {
+      $(".tabs .tabInput")[0].attr('checked', 'checked');
+    }
+  })
 })
