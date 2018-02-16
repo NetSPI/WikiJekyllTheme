6530d47b954643ed9b4bc5eecc41907c02d20e9d
Jacob Reynolds <jreynoldsdev@gmail.com>
2018-02-15 20:58:50 -0600

    Update tab.js

--- assets/javascripts/tab.js
+++ assets/javascripts/tab.js
@@ -16,7 +16,19 @@ $(document).ready(function() {
 
   //If there's no hash set a default one
   //Same if there's a hash that we don't have
-  if (!window.location.hash || !$(".tabs .tabInput#" + window.location.hash.replace('#', '')).length) {
+  if (!window.location.hash) {
+    if (areTabsOnPage() && !doesHashExistOnPage()) {
     window.location.hash = $(".tabs .tabInput")[0].id;
+    }
+  }
+  
+  //Are there tabs on the page
+  function areTabsOnPage() {
+   return $(".tabs .tabInput).length>0);
+  }
+    
+  //Does the current hash have a matching tab on the page
+  function doesHashExistOnPage() {
+    return ($(".tabs .tabInput#" + window.location.hash.replace('#', '')).length>0)
   }
 })
